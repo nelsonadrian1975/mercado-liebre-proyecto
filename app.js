54f6940fb9736814abedd31be0466f7d0d8de539
Adrián Arca <arcagodoy@gmail.com>
2022-03-30 21:24:02 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const path = require('path')
 
+
+app.listen(PORT)
 app.use(express.static(path.resolve(__dirname, "public")))
 app.listen(PORT, () => console.log('Servidor corriendo en el puerto ' + PORT))
 
-
-/*app.get('/contacto', function (req, res) {
-    res.send('Dejanos tu contacto')
-})
-
-app.get('/un-array', function (req, res) {
-    res.send([1, 2, 3, 4, 5])
-})
-
-app.get('/un-objeto', function (req, res) {
-    res.send({ name: 'Dario' })
-})*/
 
 app.get('/', function (req, res) {
     let htmlPath = path.resolve(__dirname, './views/home.html');
